45010095ccd444c03d074e19e920d0d6e6e96160
lemon1990 <misha.skopenko@yandex.ru>
2022-11-25 18:57:58 +0400

    testing test

--- server/src/tests/auth.test.js
+++ server/src/tests/auth.test.js
@@ -1,11 +1,12 @@
-const { MongoMemoryServer } = require("mongodb-memory-server");
+import { MongoMemoryServer } from "mongodb-memory-server";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+
 import mongoose from "mongoose";
 
 import request from "supertest";
-import { MongoClient } from "mongodb";
 
 import { app } from "../helpers/runApp";
-import runMongo from "../helpers/mongo";
 
 let server;
 let mongoServer;
@@ -21,31 +22,36 @@ beforeAll(async () => {
   mongos = await mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: "test",
   });
   server = await app;
 });
 
 /* Closing database connection after each test. */
 afterEach(async () => {
-  console.log(mongos.connection.collections);
-  await mongoServer.stop();
+  // await mongoServer.stop();
 });
 
 describe("POST /auth/register", () => {
   it("register user", async () => {
     const res = await request(server)
       .post("/auth/register")
-      .send({ email: "roma.skopenko@yandedx.ru", password: "mishamisha123" });
+      .send({ email: "roda.skopenko@yandedx.ru", password: "mishamisha123" });
     expect(res.status).toBe(201);
   });
 });
 
-// describe("GET /api/products", () => {
-//   it("should return all products", async () => {
-//     const res = await request(server)
-//       .post("/auth/login")
-//       .send({ email: "misha.skopenko@yandex.ru", password: "mishamisha123" });
-//     console.log(res);
-//     expect(200).toBe(200);
-//   });
-// });
+describe("POST /auth/login", () => {
+  it("should return all products", async () => {
+    const hashedPassword = await bcrypt.hash("testtest", 10);
+    const testUser = new User({
+      email: "ronma.skopenko@yandedx.ru",
+      password: hashedPassword,
+    });
+    await testUser.save();
+    const res = await request(server)
+      .post("/auth/login")
+      .send({ email: "ronma.skopenko@yandedx.ru", password: "testtest" });
+    expect(200).toBe(200);
+  });
+});
