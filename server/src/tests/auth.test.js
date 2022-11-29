b53ecc0a11fc4aafbd1007fb524786d809b52055
lemon1990 <misha.skopenko@yandex.ru>
2022-11-29 14:34:14 +0400

    update test auth

--- server/src/tests/auth.test.js
+++ server/src/tests/auth.test.js
@@ -1,57 +1,32 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import User from "../models/User";
-import bcrypt from "bcryptjs";
-
-import mongoose from "mongoose";
-
+// import User from "../models/User";
+// import bcrypt from "bcryptjs";
+import { dbConnect, dbDisconnect } from "../helpers/dbTestingHandler";
 import request from "supertest";
-
 import { app } from "../helpers/runApp";
 
-let server;
-let mongoServer;
-let mongos;
-let dbUrl;
-// beforeEach(async () => {
-//   await mongoose.connect(process.env.MONGO);
-// });
+const testUser = {
+  email: "test.test@test.ru",
+  password: "testtest",
+};
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  dbUrl = mongoServer.getUri();
-  mongos = await mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "test",
-  });
-  server = await app;
+  await dbConnect();
 });
 
-/* Closing database connection after each test. */
-afterEach(async () => {
-  // await mongoServer.stop();
+afterAll(async () => {
+  await dbDisconnect();
 });
 
 describe("POST /auth/register", () => {
-  it("register user", async () => {
-    const res = await request(server)
-      .post("/auth/register")
-      .send({ email: "roda.skopenko@yandedx.ru", password: "mishamisha123" });
+  it("user registration successful", async () => {
+    const res = await request(app).post("/auth/register").send(testUser);
     expect(res.status).toBe(201);
   });
 });
 
 describe("POST /auth/login", () => {
-  it("should return all products", async () => {
-    const hashedPassword = await bcrypt.hash("testtest", 10);
-    const testUser = new User({
-      email: "ronma.skopenko@yandedx.ru",
-      password: hashedPassword,
-    });
-    await testUser.save();
-    const res = await request(server)
-      .post("/auth/login")
-      .send({ email: "ronma.skopenko@yandedx.ru", password: "testtest" });
-    expect(200).toBe(200);
+  it("user authorization successful", async () => {
+    const res = await request(app).post("/auth/login").send(testUser);
+    expect(res.status).toBe(200);
   });
 });
