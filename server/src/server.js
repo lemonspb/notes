e69b405172b2e20b1d58609a2a79b0e4377656d5
Михаил <todo@asd.ru>
2023-01-30 14:32:14 +0400

    delete backend

--- server/src/server.js
+++
@@ -1,9 +0,0 @@
-import { app } from "./helpers/runApp.js";
-import runMongo from "./helpers/mongo.js";
-
-void (async () => {
-  console.log("Starting mongo");
-  await runMongo();
-  console.log("Mongo connected");
-  await app.listen(process.env.PORT, () => {});
-})();
